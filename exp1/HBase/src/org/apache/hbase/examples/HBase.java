a11512312501733f5e43ffb45a04bb8875a038c0
cliche <745356675@qq.com>
2022-03-29 01:20:58 +0800

    -part 2 complete

--- exp1/HBase/src/org/apache/hbase/examples/HBase.java
+++ exp1/HBase/src/org/apache/hbase/examples/HBase.java
@@ -13,7 +13,6 @@ public class HBase {
 	private Configuration conf;
 	private Connection connection;
 	private Admin admin;
-	private Table table;
 
 	public static void main(String[] args) {
 		HBase hBase = new HBase();
@@ -24,7 +23,41 @@ public class HBase {
 					hBase.createTable(args[2], Arrays.copyOfRange(args, 3, args.length));
 					break;
 				case "add":
-					hBase.addRecord(tableName, row, fields, values);
+					// args[4]: field1,field2,field3
+					// args[5]: value1,value2,value3
+					String[] fields = args[4].split(",");
+					String[] values = args[5].split(",");
+					hBase.addRecord(args[2], args[3], fields, values);
+					break;
+				case "scan":
+					hBase.scanColumn(args[2], args[3]);
+					break;
+				case "modify":
+					hBase.modifyData(args[2], args[3], args[4], args[5]);
+					break;
+				case "deleteRow":
+					hBase.deleteRow(args[2], args[3]);
+					break;
+				case "list":
+					hBase.list();
+					break;
+				case "print":
+					hBase.print(args[2]);
+					break;
+				case "insert":
+					String[] colsInsert = args[4].split(":");
+					hBase.insert(args[2], args[3], colsInsert[0], colsInsert[1], args[5]);
+					break;
+				case "deleteCol":
+					String[] colsDelete = args[4].split(":");
+					hBase.deleteCol(args[2], args[3], colsDelete[0], colsDelete[1]);
+					break;
+				case "clear":
+					hBase.clear(args[2]);
+					break;
+				case "count":
+					hBase.count(args[2]);
+					break;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -104,9 +137,10 @@ public class HBase {
 		connect();
 		Table table = connection.getTable(TableName.valueOf(tableName));
 		Scan scan = new Scan();
-		scan.addFamily(Bytes.toBytes(column));
+		String[] cols = column.split(":");
+		scan.addColumn(cols[0].getBytes(), cols[1].getBytes());
 		ResultScanner scanner = table.getScanner(scan);
-		for (Result result = scanner.next(); result != null; result = scanner.next())
+		for (Result result : scanner)
 			printCell(result);
 		table.close();
 		disconnect();
@@ -115,33 +149,119 @@ public class HBase {
 	public void printCell(Result result) {
 		Cell[] cells = result.rawCells();
 		for (Cell cell : cells) {
-			System.out.println("RowName: " + new String(CellUtil.cloneRow(cell)) + " ");
-			System.out.println("Timestamp: " + cell.getTimestamp() + " ");
-			System.out.println("column Family: " + new String(CellUtil.cloneFamily(cell)) + " ");
-			System.out.println("row Name: " + new String(CellUtil.cloneQualifier(cell)) + " ");
-			System.out.println("value: " + new String(CellUtil.cloneValue(cell)) + " ");
+			System.out.println("rowName: " + new String(CellUtil.cloneRow(cell)));
+			System.out.println("column Family: " + new String(CellUtil.cloneFamily(cell)));
+			System.out.println("column: " + new String(CellUtil.cloneQualifier(cell)));
+			System.out.println("value: " + new String(CellUtil.cloneValue(cell)));
+			System.out.println("timestamp: " + cell.getTimestamp());
 		}
 	}
 
 	/* 
 	 * 修改表 tableName，行 row(可以用学生姓名 S_Name 表示)，列 column 指定的单元格的数据
 	 */
-	public void modifyData(String tableName, String row, String family, String column, String value) throws IOException {
+	public void modifyData(String tableName, String row, String column, String value) throws IOException {
 		connect();
 		Table table = connection.getTable(TableName.valueOf(tableName));
 		Put put = new Put(row.getBytes());
-		put.addColumn(family.getBytes(), column.getBytes(), value.getBytes());
+		String[] cols = column.split(":");
+		put.addColumn(cols[0].getBytes(), cols[1].getBytes(), value.getBytes());
 		table.put(put);
 		table.close();
 		disconnect();
 	}
 
+	/*
+	 * 删除表 tableName 中 row 指定的行的记录
+	 */
 	public void deleteRow(String tableName, String row) throws IOException {
 		connect();
 		Table table = connection.getTable(TableName.valueOf(tableName));
 		Delete delete = new Delete(row.getBytes());
 		table.delete(delete);
 		table.close();
+		disconnect();
+	}
+
+	/*
+	 *  (1) 列出 HBase 所有的表的相关信息，例如表名
+	 */ 
+	public void list() throws IOException {
+		connect();
+		HTableDescriptor[] hTableDescriptors = admin.listTables();
+		for (HTableDescriptor hTableDescriptor : hTableDescriptors)
+			System.out.println("TableName: " + hTableDescriptor.getNameAsString());
+		disconnect();
+	}
+
+	/*
+	 * (2) 在终端打印出指定的表的所有记录数据
+	 */
+	public void print(String tableName) throws IOException {
+		connect();
+		Table table = connection.getTable(TableName.valueOf(tableName));
+		Scan scan = new Scan();
+		ResultScanner scanner = table.getScanner(scan);
+		for (Result result : scanner)
+			printCell(result);
+		
+		disconnect();
+	}
+
+	/* 
+	 * (3) 向已经创建好的表添加和删除指定的列族或列
+	 */
+	public void insert(String tableName, String rowKey, String columnFamily, String column, String value) 
+			throws IOException {
+		connect();
+		Table table = connection.getTable(TableName.valueOf(tableName));
+		Put put = new Put(rowKey.getBytes());
+		put.addColumn(columnFamily.getBytes(), column.getBytes(), value.getBytes());
+		table.put(put);
+		table.close();
+		disconnect();
+	}
+
+	public void deleteCol(String tableName, String rowKey, String columnFamily, String column) 
+			throws IOException {
+		connect();
+		Table table = connection.getTable(TableName.valueOf(tableName));
+		Delete delete = new Delete(rowKey.getBytes());
+		// 删除指定列族
+		delete.addFamily(Bytes.toBytes(columnFamily));
+		// 删除指定列
+		delete.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(column));
+		table.delete(delete);
+		table.close();
+		disconnect();
+	}
+
+	/*
+	 * (4) 清空指定的表的所有记录数据
+	 */
+	public void clear(String tableName) throws IOException {
+		connect();
+		TableName tName = TableName.valueOf(tableName);
+		HTableDescriptor hTableDescriptor = admin.getTableDescriptor(tName);
+		admin.disableTable(tName);
+		admin.deleteTable(tName);
+		admin.createTable(hTableDescriptor);
+		disconnect();
+	}
+
+	/*
+	 * (5) 统计表的行数
+	 */ 
+	public void count(String tableName) throws IOException {
+		connect();
+		Table table = connection.getTable(TableName.valueOf(tableName));
+		Scan scan = new Scan();
+		ResultScanner scanner = table.getScanner(scan);
+		int num = 0;
+		for (Result result : scanner)
+			num++;
+		System.out.println("count: " + num);
+		scanner.close();
 		disconnect();
 	}
 }
