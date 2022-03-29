a14644cf66df510ed1abde4372aa1c375a287817
cliche <745356675@qq.com>
2022-03-29 22:34:52 +0800

    -mongo complete

--- exp1/Others/mongodb/src/examples/mongodb/MongoDBDemo.java
+++ exp1/Others/mongodb/src/examples/mongodb/MongoDBDemo.java
@@ -1,14 +1,34 @@
 package examples.mongodb;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+
+import org.bson.Document;
+
 public class MongoDBDemo {
-
-	public MongoDBDemo() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			System.out.println("connecting...");
+			MongoClient mongoClient = new MongoClient("localhost", 27017);
+			MongoDatabase mongoDatabase = mongoClient.getDatabase("exp1");
+			System.out.println("Successfully connected");
+			// insert document
+			MongoCollection<Document> collection = mongoDatabase.getCollection("Student");
+			Document document = new Document("name", "Scofield").append("score",
+					new Document("English", 45).append("Math", 89).append("Computer", 100));
+			collection.insertOne(document);
+			System.out.println("Successfully inserted");
+			// query 'Scofield' info
+			MongoCursor<Document> cursor = collection.find(new Document("name", "Scofield"))
+								.projection(new Document("score", 1).append("_id", 0)).iterator();
+			while (cursor.hasNext())
+				System.out.println(cursor.next().toJson());
+			mongoClient.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
