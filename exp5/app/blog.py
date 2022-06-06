c1b727aad72e83d77cc67d616517487930ee83c4
cliche <745356675@qq.com>
2022-06-06 19:44:29 +0800

    -

--- exp5/app/blog.py
+++ exp5/app/blog.py
@@ -1,5 +1,8 @@
 from codecs import getdecoder
-from django.shortcuts import render
+from operator import methodcaller
+from pydoc import getpager
+from webbrowser import get
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -10,6 +13,7 @@ from app.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+# 主页面
 @bp.route('/')
 def index():
     db = get_db()
@@ -20,3 +24,75 @@ def index():
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
+# 新建blog
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+    if not title:
+        error = 'This is required'
+    
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'INSERT INTO post (title, body, author_id)'
+            ' VALUES (? ? ?)',
+            (title, body, g.user['id'])
+        )
+        db.commit()
+        return redirect(url_for('blog.index'))
+    
+    return render_template('blog/create.html')
+
+# 获取对应id的博客post
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id, )
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    
+    return post
+
+# 更新blog
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.methos == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+    
+        if not title:
+            error = 'Title is required.'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                'WHERE id = ?'
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    
+    return render_template('blog/update.html', post=post)
+
+# 删除blog
