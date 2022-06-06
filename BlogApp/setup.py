e9514ceed3a9d261e1d7a014b905bcd42fae184b
cliche <745356675@qq.com>
2022-06-06 20:59:01 +0800

    - BlogApp Init exp5

---
+++ BlogApp/setup.py
@@ -0,0 +1,14 @@
+from struct import pack
+from setuptools import find_packages, setup
+
+setup(
+    name='BlogApp',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
+
