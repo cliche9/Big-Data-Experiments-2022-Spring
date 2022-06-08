3ed045798fa6bc39a865a7fda6392bb12658b5da
cliche <745356675@qq.com>
2022-06-08 12:28:12 +0800

    -

---
+++ exp5/app/__init__.py
@@ -0,0 +1,36 @@
+"""
+1. 包含应用工厂
+2. 告诉python: app应视为包
+"""
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # 创建/设置 app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'movie.sqlite')
+    )
+
+    if test_config is None:
+        # 非测试模式下导入设置
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # 导入测试设置
+        app.config.from_mapping(test_config)    
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db, movie
+    # app注册数据库初始化函数
+    db.init_app(app)
+    # app注册电影movie蓝图
+    app.register_blueprint(movie.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
